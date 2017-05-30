374608fefe85e63ef1778a0bb556f61104bb1a5e
saninshakya <saninshakya@gmail.com>
2017-05-30 14:03:53 +0700

    add comments

--- maqe_bot.php
+++ maqe_bot.php
@@ -1,32 +1,48 @@
 <?php
-$string = $argv[1];
+// get the string passed from the cmd
+// $string = $argv[1];
+$string = 'RRW11RLLW19RRW12LW1';
+/*
+* convert the string into array.
+* @$x, $y are set the default co-ordinates of Bot
+* @$direction Bot is facing towards default 'North'
+* @$walk is set false at beginning and is set true when 'W' key is found.
+*/
 preg_match_all('/\d+|\w/', $string, $matches);
 $x = 0;
 $y = 0;
 $direction = 'North';
-
+$walk = false;
 foreach ($matches as $match) {
     foreach ($match as $value) {
+        /*
+        * check if the value is number or alphabet
+        * check if the previous alphabet was 'W'
+        * walk the number of steps along the given direction.
+        */
         if(is_numeric($value) && $value > 0){
-            if($direction == 'East'){
-                $x = $x + $value;
-            }
-            if($direction == 'North'){
-                $y = $y + $value;
-            }
-            if($direction == 'South'){
-                $y = $y - $value;
-            }
-            if($direction == 'West'){
-                $x = $x - $value;
-                if($x < 0){
-                    $x = -$x;
+            if($walk == true)
+            {
+                if($direction == 'East'){
+                    $x = $x + $value;
+                }
+                if($direction == 'North'){
+                    $y = $y + $value;
+                }
+                if($direction == 'South'){
+                    $y = $y - $value;
+                }
+                if($direction == 'West'){
+                    $x = $x - $value;
                 }
             }
+            $walk = false;
         }
         else
         {
+            // convert the alphabet to uppercase
             $value = strtoupper($value);
+            //check if the alphabet is 'R' or 'L' and rotate the Bot
             if($value == 'R'){
                 if($direction == 'North'){
                     $direction = 'East';
@@ -35,9 +51,9 @@ foreach ($matches as $match) {
                     $direction = 'South';
                 }
                 elseif ($direction == 'South'){
-                    $direction == 'West';
+                    $direction = 'West';
                 }
-                else{
+                elseif ($direction == 'West'){
                     $direction = 'North';
                 }
             }
@@ -49,13 +65,15 @@ foreach ($matches as $match) {
                     $direction = 'North';
                 }
                 elseif ($direction == 'South'){
-                    $direction == 'East';
+                    $direction = 'East';
                 }
-                else{
+                elseif ($direction == 'West'){
                     $direction = 'South';
                 }
             }
-            else if($value == 'West'){
+            //if W is found continue the loop and set the $walk as true
+            else if($value == 'W'){
+                $walk = true;
                 continue;
             }
         }
